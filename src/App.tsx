3e9f0d6091ca7498cf1da542a95b8c35b1b45303
Tommy <yuli9223@colorado.edu>
2025-02-25 17:49:41 +0800

    feat: add SpeedInsights package

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import "@/locales";
-import { RouterProvider } from "react-router";
-import router from "./routes";
+import '@/locales';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { RouterProvider } from 'react-router';
+import router from './routes';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default App;
